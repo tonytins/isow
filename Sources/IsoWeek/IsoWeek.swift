b61c0910e0230951322fd8356dcce595799634cf
Tony Bark <35226681+tonytins@users.noreply.github.com>
2026-05-06 22:54:51 -0400

    Enforce ISO rules

--- Sources/IsoWeek/IsoWeek.swift
+++ Sources/IsoWeek/IsoWeek.swift
@@ -12,12 +12,16 @@ struct IsoWeek: ParsableCommand {
     mutating func run() throws {
         // Initialize date and calendar
         let date = Date()
-        let cal = Calendar(identifier: .gregorian)
+        var cal = Calendar(identifier: .iso8601)
 
         // Grab calendar data
         let week = cal.component(.weekOfYear, from: date)
         let year = cal.component(.year, from: date)
         let day = cal.component(.day, from: date)
+
+        // ISO rules
+        cal.firstWeekday = 2
+        cal.minimumDaysInFirstWeek = 4
 
         let isoCal = String(format: "%d-W%02d-%02d", year, week, day)
 
